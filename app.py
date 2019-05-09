cebdfb584ff8cb7e952049743b9e4c5075cd4338
MatthewDodi <matthew.dodi@gmail.com>
2019-05-08 21:35:54 -0700

    type: feat - Created 5 endpoints

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Hello World
+# @app.route('/')
+# def home():
+#     return 'Hello World!'
 
-@app.route('/')  # home
-def home():
-    return 'Hello World!'
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store_by_name(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_store():
+    pass
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_store_item(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_store_item(name):
+    pass
 
 
 app.run(port=5000)
