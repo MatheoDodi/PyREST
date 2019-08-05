528f40ec1030c1e3c6f69540143cbeae74435df4
MatthewDodi <matthew.dodi@gmail.com>
2019-08-04 22:53:40 -0700

    Added get_single_item method

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'First',
+        'items': [
+            {
+                'name': 'An item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
 # Hello World
 # @app.route('/')
@@ -10,28 +22,55 @@ app = Flask(__name__)
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    resp = jsonify(success=True)
+    return resp
 
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store_by_name(name):
-    pass
+    store_found = list(filter(lambda store: store['name'] == name, stores))
+    if bool(len(store_found)):
+        return jsonify(store_found[0])
+    else:
+        return jsonify({'message': 'Store not found'})
 
 # GET /store
 @app.route('/store')
-def get_store():
-    pass
+def get_stores():
+    return jsonify(stores)
 
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_store_item(name):
-    pass
+    store_found = list(filter(lambda store: store['name'] == name, stores))
+    if bool(len(store_found)):
+        store = store_found[0]
+        request_data = request.get_json()
+        new_item = {
+            'name': request_data['name'],
+            'price': request_data['price']
+        }
+        store['items'].append(new_item)
+        return jsonify(store)
+    else:
+        return jsonify({'message': 'Store not found'})
+
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
-def get_store_item(name):
-    pass
+def get_store_items(name):
+    store_found = list(filter(lambda store: store['name'] == name, stores))
+    if bool(len(store_found)):
+        return jsonify({'items': store_found[0]['items']})
+    else:
+        return jsonify({'message': 'Store not found'})
 
 
 app.run(port=5000)
